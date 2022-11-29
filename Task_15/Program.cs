094bbb118507b65d8ddd3dad98bc6cecd460c34a
Valeria <valeria_korzun@mail.ru>
2022-11-29 23:49:31 +0300

    Выходной день или нет

---
+++ Task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Clear();
+int n = int.Parse(Console.ReadLine());
+while (n <1 || n > 8) 
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1) Console.Write("нет");
+else if (n == 2) 
+    Console.Write("нет");
+else if (n == 3) 
+    Console.Write("нет");
+else if (n == 4) 
+    Console.Write("нет");
+else if (n == 5) 
+    Console.Write("нет");
+else if (n == 6) 
+    Console.Write("Выходной");
+else    Console.Write("Выходной");
