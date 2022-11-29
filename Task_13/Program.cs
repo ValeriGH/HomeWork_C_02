872dad6a1bdadd94c465b17005c94e892208d862
Valeria <valeria_korzun@mail.ru>
2022-11-29 23:49:01 +0300

    Нахождение третьей цифры

---
+++ Task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    int rever = 0;
+    while (num > 99)
+    {
+        rever = rever * 10 + num % 10;
+        num /= 10;
+        //Console.WriteLine(rever);
+    }
+    Console.WriteLine(rever % 10);
+}
