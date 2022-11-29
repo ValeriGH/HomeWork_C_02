addc4f80c420022f016955b09e601359eccdf5ae
Valeria <valeria_korzun@mail.ru>
2022-11-29 23:49:58 +0300

    Нахождение второго максимума

---
+++ Task_SecondMax/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дана последовательность натуральных чисел. Найти второй максимум
+Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int firstMax = n;
+int secondMax = -1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > firstMax)
+    {
+        secondMax = firstMax;
+        firstMax = n;
+    }
+    else if (n > secondMax)
+    {
+        secondMax = n;
+    }
+}
+Console.Write(secondMax);
